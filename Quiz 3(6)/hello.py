dc7fcb077b7425b36a4c94c993869d96545d9295
xysecurity <41580204+xysecurity@users.noreply.github.com>
2019-06-18 19:22:08 -0500

    1

---
+++ Quiz 3(6)/hello.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+from flask import Flask
+from io import BytesIO
+import base64
+import os
+import csv
+from flask import render_template
+from flask import request
+import sqlite3
+import random
+import time
+import redis
+
+app = Flask(__name__)
+
+# print(os.getenv("PORT"))
+port = int(os.getenv("PORT", 5000))
+print(port)
+db=sqlite3.connect('2.db')
+cu=db.cursor()
+data=cu.execute('select * from quake6')
+r=redis.Redis(host='anakin.redis.cache.windows.net',port=6379,password='QuhFzLTrVd+LNdWaF1fAxikpgc6bdfiaimiqk2PJB44=',decode_responses=True)
+data2=[]
+for row in data:
+	data2.append(row)
+db.commit()
+db.close()
+
+@app.route('/choose')
+def choose():
+	start_dep=float(request.args.get('start_dep'))
+	end_dep=float(request.args.get('end_dep'))
+	lon=float(request.args.get('longitude'))
+	# query_number=float(request.args.get('query_num'))
+	db=sqlite3.connect('2.db')
+	cu=db.cursor()
+	data=cu.execute('select * from quake6 where depthError between %d and %d and longitude > %d' %(start_dep,end_dep,lon))
+	list2=[]
+	for row in data:
+		# print(row[4])
+		list2.append(row)
+
+
+	return render_template('earchquake.html',data=list2)
+	db.commit()
+	db.close()
+	
+	# return 'no picture available'
+@app.route('/choose2')
+def choose2():
+	start_lat=float(request.args.get('start_dep'))
+	end_lat=float(request.args.get('end_dep'))
+	query_number=int(request.args.get('query_num'))
+	db=sqlite3.connect('2.db')
+	cu=db.cursor()
+	list2=[]
+	for i in range(1,query_number):
+		a=0
+		b=0
+		while a==b:
+			a=round(random.uniform(start_lat,end_lat),1)
+			b=round(random.uniform(start_lat,end_lat),1)		
+		if a>b:
+			big=a
+			small=b
+		else:
+			big=b
+			small=a
+		start=time.time()
+		data=cu.execute('select count(time) from quake6 where depthError between %d and %d' %(small,big))
+		end=time.time()
+		cost_time=end-start
+		for cot in data:
+			count=cot[0]
+		list2.append([count,small,big,cost_time])
+
+
+	return render_template('querytime.html',data=list2) 	
+
+
+@app.route('/choose3')
+def choose3():
+	start_lat=float(request.args.get('start_dep'))
+	end_lat=float(request.args.get('end_dep'))
+	query_number=int(request.args.get('query_num'))
+	db=sqlite3.connect('2.db')
+	cu=db.cursor()
+	
+	list2=[]
+	for i in range(1,query_number):
+		a=0
+		b=0
+		count=0
+		while a==b:
+			a=round(random.uniform(start_lat,end_lat),1)
+			b=round(random.uniform(start_lat,end_lat),1)		
+		if a>b:
+			big=a
+			small=b
+		else:
+			big=b
+			small=a
+		start=time.time()
+		i=0
+		for i in range(200):
+			# print(r.get('a%d'%i))
+			if float(r.get('a%d'%i))>=small and float(r.get('a%d'%i))<big:
+				print(r.get('a%d'%i))
+				count+=1
+		end=time.time()
+		cost_time=end-start
+
+		list2.append([count,small,big,cost_time])
+
+
+	return render_template('querytime.html',data=list2) 	
+
+
+
+@app.route('/',methods=['POST','GET'])
+def input():
+	if request.args.get('start_dep') and request.args.get('end_dep') and request.args.get('query_num') and request.args.get('choice'):
+		start_lat=float(request.args.get('start_dep'))
+		end_lat=float(request.args.get('end_dep'))
+		query_number=int(request.args.get('query_num'))
+		choice=int(request.args.get('choice'))
+		db=sqlite3.connect('2.db')
+		cu=db.cursor()
+		list2=[]
+		if choice==1:
+			start=time.time()
+			for i in range(1,query_number):
+				a=0
+				b=0
+				while a==b:
+					a=round(random.uniform(start_lat,end_lat),1)
+					b=round(random.uniform(start_lat,end_lat),1)		
+				if a>b:
+					big=a
+					small=b
+				else:
+					big=b
+					small=a
+				data=cu.execute('select count(time) from quake6 where latitude between %d and %d' %(small,big))
+
+			end=time.time()
+			cost_time=end-start
+		else:
+			start=time.time()
+			for i in range(1,query_number):
+				a=0
+				b=0
+				count=0
+				while a==b:
+					a=round(random.uniform(start_lat,end_lat),1)
+					b=round(random.uniform(start_lat,end_lat),1)		
+				if a>b:
+					big=a
+					small=b
+				else:
+					big=b
+					small=a			
+				for row in data2:
+					if row[9] !=None:	
+						if row[9]>=small and row[9]<big:
+							count+=1
+			end=time.time()
+			cost_time=end-start
+		return render_template('form.html',cost_time=cost_time)
+	return render_template('form.html')
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=port,debug=True)
---
+++ Quiz 3(6)/utc.py
@@ -0,0 +1,60 @@
+# def calctime(curlon):
+# 	shangValue=int(curlon//15)
+# 	yuValue=float(curlon%15)
+# 	if yuValue<=7.5 and yuValue>-7.5:
+# 		return shangValue
+# 	else:
+# 		if yuValue<=-7.5:
+# 			return shangValue-1
+# 		else:
+# 			return shangValue+1
+
+
+# print(calctime(22.5))
+# print('08'>'17')
+# print(int('08'))
+
+list=[]
+i=0
+a=0
+j=0
+
+# for i in range(90):
+# 	for j in range(180):
+# 		list.append([i,i+1,j,j+1,0])
+# a=[80,110]
+# print(len(list))
+# print(list2)
+
+# while i != 90:
+# 	j=0
+# 	while j !=180:
+# 		list.append([i,i+5,j,j+5,0])
+# 		j+=5
+# 	i+=5
+
+#  print (list)
+
+# for i in range(len(list)):
+# 	if (a[0]>=list[i][0] and a[0]<list[i][1]) and (a[1]>=list[i][2] and a[1]<list[i][3]):
+# 		list[i][4]+=1
+# 		print(list[i])
+
+# print(min('test','tes'))
+# print(float(0.2))
+import sqlite3
+import redis
+r=redis.Redis(host='anakin.redis.cache.windows.net',port=6379,password='QuhFzLTrVd+LNdWaF1fAxikpgc6bdfiaimiqk2PJB44=',decode_responses=True)
+db=sqlite3.connect('2.db')
+cu=db.cursor()
+data=cu.execute('select * from quake6')
+i=0
+# for row in data:
+# 	r.set('a%d'%i,row[9])
+# 	i+=1
+# 	print(row[9])
+print(r.get('a2'))
+for i in range(10761):
+	if r.get('a%d'%i)!=None:
+		print(r.get('a%d'%i))
+
