63c69353de08c48d0e325e520abd963b6b761524
xysecurity <41580204+xysecurity@users.noreply.github.com>
2019-06-20 00:08:05 -0500

    1

---
+++ Assign4/test.py
@@ -0,0 +1,15 @@
+# count=[[1,5,0],[5,10,0],[10,20,0],[20,50,0],[50,100,0]]
+# if 65>count[4][0] and 90<count[4][1]:
+# 	count[4][2]+=1
+
+# print(range(len(count)))
+# for i in range(len(count)):
+# 	print (count[i])
+# print(65>count[4][0] and 110<count[4][1])
+import random
+import sqlite3
+import redis
+r=redis.Redis(host='anakin.redis.cache.windows.net',port=6379,password='QuhFzLTrVd+LNdWaF1fAxikpgc6bdfiaimiqk2PJB44=',decode_responses=True)
+# a=round(random.uniform(0,9),1)
+# print(a)
+
